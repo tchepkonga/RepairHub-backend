52ae0ebfbb9752177cb028a24c92e0fb3508c0ec
Titus Kipngetich Chepkonga <TChepkonga@safaricom.co.ke>
2023-08-06 16:21:56 +0300

    GET, PUT, or DELETE requests test

--- OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/routes/orders.js
+++ OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/routes/orders.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 
-// Require the Order model we just created
+// Require the Order model
 const Order = require('../models/order');
 
-router.post('/', async (req, res) => {
-  let order = new Order({
-    customerName: req.body.customerName,
-    device: req.body.device,
-    issue: req.body.issue
-  });
-  
-  order = await order.save();
+const upload = multer({ dest: 'uploads/' });
 
-  res.send(order);
+router.post('/', upload.single('signedDocument'), async (req, res) => {
+  try {
+    // Check if the file was uploaded
+    if (!req.file || !req.file.path) {
+      return res.status(400).send("File not uploaded correctly.");
+    }
+
+    const order = new Order({
+      customerName: req.body.customerName,
+      device: req.body.device,
+      issue: req.body.issue,
+      status: 'Received',
+      signedDocument: req.file.path
+    });
+
+    const result = await order.save();
+    res.send(result);
+  } catch (error) {
+    console.log("Error saving the order:", error);
+    res.status(400).send(error.message);
+  }
 });
 
 module.exports = router;
--- OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/server.js
+++ OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const orders = require('./routes/orders');
+
+const upload = multer({ dest: 'uploads/' });
+
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1/repairhub', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+app.use(express.json());
 
+mongoose.connect('mongodb://127.0.0.1/repairhub', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.use('/api/orders', orders);
 
 app.listen(port, () => {
   console.log(`RepairHub app listening at http://localhost:${port}`)
