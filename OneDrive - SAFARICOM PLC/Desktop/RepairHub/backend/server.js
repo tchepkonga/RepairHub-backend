613b333ca950da8bff20a915173ef63132c00235
Titus Kipngetich Chepkonga <TChepkonga@safaricom.co.ke>
2023-08-15 00:13:37 +0300

    environment variables for security and  updated server.js to use the environment variables and mongoose.connect updated

--- OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/server.js
+++ OneDrive - SAFARICOM PLC/Desktop/RepairHub/backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
@@ -8,7 +9,8 @@ const rateLimit = require("express-rate-limit"); // Add rate-limiter
 const upload = multer({ dest: 'uploads/' });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middlewares
 app.use(express.json());
@@ -26,14 +28,18 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/login', require('./routes/login'));
 app.use('/api/orders', orders);
 
-mongoose.connect('mongodb://127.0.0.1/repairhub', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 app.listen(port, () => {
   console.log(`RepairHub app listening at http://localhost:${port}`)
 });
